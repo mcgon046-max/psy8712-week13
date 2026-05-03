cc0bfb2225d3b3d3a6b78f19ddf09321d333ab35
Patrick Mcgonagle <mcgon046@umn.edu>
2026-05-03 13:38:18 -0500

    save

--- R/week13-dplyr.R
+++ R/week13-dplyr.R
@@ -9,8 +9,14 @@ library(RPostgres)
 NEON_USER <- Sys.getenv("NEON_USER")
 NEON_PW <- Sys.getenv("NEON_PW")
 
-## Connecting to SQL server 
-sql_connect <- dbConnect(
-  
+## Connecting to SQL server (PostgreSQL database)
+sql_connect <- DBI::dbConnect(
+  RPostgres::Postgres(),
+  dbname = "neondb", # Database name
+  host = "ep-billowing-union-am14lcnh-pooler.c-5.us-east-1.aws.neon.tech", # Host from string in assignment 
+  port = 5432, # port from string in assignment 
+  user = NEON_USER, # Defined above
+  password = NEON_PW, # defined above 
+  sslmode = "require" # Requires SSL 
 )
   
