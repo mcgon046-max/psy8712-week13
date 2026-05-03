94648db58de13e1da647775055a2af87d602f0b3
Patrick Mcgonagle <mcgon046@umn.edu>
2026-05-03 13:34:54 -0500

    Save

--- R/week13-dplyr.R
+++ R/week13-dplyr.R
@@ -4,3 +4,13 @@ library(DBI)
 library(RPostgres)
 
 # Data Import and Cleaning 
+
+## Credentials (needed so username and password are not displayed when committing)
+NEON_USER <- Sys.getenv("NEON_USER")
+NEON_PW <- Sys.getenv("NEON_PW")
+
+## Connecting to SQL server 
+sql_connect <- dbConnect(
+  
+)
+  
